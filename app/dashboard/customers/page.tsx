a29c2557be7f9432e1c6fd986237763ede49a852
Harsh Dodiya <harsh.dodiya@achyutlabs.com>
2025-05-24 14:20:04 +0530

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomersPage = () => {
+  return (
+    <div>customers</div>
+  )
+}
+
+export default CustomersPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvoicesPage = () => {
+  return (
+    <div>invoices</div>
+  )
+}
+
+export default InvoicesPage
